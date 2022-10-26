b8dff5ed31480d969f6e054a7792874fc32ca461
Camp2507 <cabranjes@gmail.com>
2022-10-25 21:53:53 -0300

    exercicio 2

---
+++ Exercicio 2.py
@@ -0,0 +1,10 @@
+N1= 20
+N2= 20.5
+N3= 30
+
+print("Digite input para Atribuir um valor ")
+print(type (N1))
+print("Digite int para converter para um valor inteiro ")
+print(type (N2))
+print("Digite int para converter para um valor inteiro ")
+print(type (N3))
