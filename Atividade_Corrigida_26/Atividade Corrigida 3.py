07c4fdee9c51119378e3f6dde7385030deadc2a9
Camp2507 <cabranjes@gmail.com>
2022-10-26 20:17:39 -0300

    Atividade 1,2 e 3 corrigida

---
+++ Atividade_Corrigida_26/Atividade Corrigida 3.py
@@ -0,0 +1,23 @@
+
+
+print("Programa para calcular operações matematicas")
+num1 = float(input("Digite o primeiro numero inteiro: "))
+num2 = float(input("Digite o segundo numero inteiro: "))
+
+resultado = num1 + num2
+print("A soma de numero 1 + numero 2 é: ", resultado)
+
+resultado1 = num1  - num2
+print("A subtração de numero 1 - numero 2 é: ", resultado1)
+
+resultado2 = num1  * num2
+print("A multipicação de numero 1 * numero 2 é: ", resultado2)
+
+resultado3 = num1  / num2
+print("A divisão de numero 1 / numero 2 é: ", resultado3)
+
+resultado4 = num1  % num2
+print("O resto da divisão de numero 1 / numero 2 é: ", resultado4)
+
+resultado5 = num1  ** num2
+print("A Potencia de numero 1 pelo numero 2 é: ", resultado5)
---
+++ Atividade_Corrigida_26/Exercicio Corrigido 1.py
@@ -0,0 +1,12 @@
+a = 10
+b = 10.5
+c = "Camila"
+d = True
+print("O valor da variavel a é: " +str(a) + " e o tipo dela é")
+print(type(a))
+print ("O valor da variavel a é: " +str(b) + " e o tipo dela é")
+print(type(b))
+print ("O valor da variavel a é: " +str(c) + " e o tipo dela é")
+print(type(c))
+print ("O valor da variavel a é: " +str(d) + " e o tipo dela é")
+print(type(d))
