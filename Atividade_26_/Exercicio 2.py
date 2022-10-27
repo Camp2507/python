1b9777519aa8d66df306e22cf4ab879ec566d97a
Camp2507 <cabranjes@gmail.com>
2022-10-26 21:57:11 -0300

    Atividade 1,2

---
+++ Atividade_26_/Exercicio 2.py
@@ -0,0 +1,12 @@
+#Crie um programa que receba um numero digitado pelo usuário, que será comparado se o numero digitado é
+# maior que 10, igual a 10 ou menor que 10.
+
+num1 = (input("Digite o primeiro numero inteiro: "))
+
+resultado = num1 < 10
+print("Digite se o numero é < 10: ", resultado)
+resultado2 = num1 = 10
+print("Digite se o numero é = a 10: ", resultado2)
+resultado3 = num1 > 10
+print("Digite se o numero é > que 10: ", resultado3)
+
---
+++ Atividade_26_/Exercicio_1.py
@@ -0,0 +1,28 @@
+
+
+Iphone = 2980,00
+print("O preço do iphone é: ", Iphone)
+Iphone =input("Digite quantos Iphone você quer: ")
+
+Samsung = 2540,00
+print("O preço do Samsung é: ", Samsung)
+Samsung =input("Digite quantos Samsung você quer: ")
+
+Tablet = 1950,00
+print("O preço do Tablet é: ", Tablet)
+Tablet =input("Digite quantos Tablet você quer: ")
+
+PS5 = 2100,00
+print("O preço do PS5 é: ", PS5)
+PS5 =input("Digite quantos PS5 você quer: ")
+
+Notebook = 2350,00
+print("O preço do Notebook é: ", Notebook)
+Notebook =input("Digite quantos Notebook você quer: ")
+
+
+soma= Notebook + PS5 + Samsung + Tablet + Iphone
+print("Total deu: ", soma)
+
+
+
