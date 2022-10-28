bf697208b50ad08127324bb926f01096ea5c5792
Camp2507 <cabranjes@gmail.com>
2022-10-28 20:00:42 -0300

    exercicios 1, 2 e 3

--- Atividade_27_/Exercicio 3.py
+++ Atividade_27_/Exercicio 3.py
@@ -5,7 +5,9 @@
 #Repetição usando ”ENQUANTO”
 #while == “valor”
 #sendo verdadeiro repete até ser falso
-
+sim = "S"
+while sim == "S":
+    
 print("Calculadora")
 print("Digite - para subtração")
 print("Digite + Para soma")
@@ -13,6 +15,7 @@ print("Digite * Multiplicação")
 print("Digite / Divisão")
 print("Digite s para reiniciar a calculadora")
 num1 = float(input("Digite um numero real para calcular: "))
-s = "sim = reiniciar"
-while
 
+
+
+sim = input("Deseja continuar? s ou n: ").upper()
